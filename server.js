1bee1c04c098f90f6c88f0e26752008a2965b726
Johnty <johntysimon90@gmail.com>
2018-04-05 10:32:11 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,57 @@
+let express = require('express');
+let hbs = require('hbs');
+let fs = require('fs');
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+app.set('view engine','hbs');
+
+app.use((req,res,next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n' , (err) =>{
+    if(err)
+     console.log("Unable to log the request");
+
+     next();
+  });
+});
+app.use('/' , (req,res) => {
+  res.render('maintainence.hbs',{
+    pageTitle:'Maintanence Page',
+    heading:"Will be back soon",
+    desc:"Site is currently under maintainence"
+  })
+
+});
+
+app.use(express.static(__dirname + ' /public'));
+app.get('/',  (req, res) => {
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcome:"Welcome to the Home page"
+  });
+})
+
+app.get('/about',(req,res) =>{
+  res.render('about.hbs',{
+    pageTitle:'About Page',
+    currentYear : new Date().getFullYear()
+  });
+})
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMessage:"Unable to connect to page"
+  });
+})
+
+app.listen(3000,() =>{
+  console.log('Server is up and running on port 3000');
+});
